1bbc0e90a90c3d953a3f454416bc984b9c49c20b
Bhargav <bhargav@axivise.in>
2023-11-06 12:31:05 +0530

    new changes

--- data.js
+++ data.js
@@ -1,8 +1,39 @@
 const form = document.getElementById("Project-form");
 const table = document.getElementById("dataTable");
 
+let currentPage = 1;
+const itemsPerPage = 10;
+
+function showPage(page) {
+    while (table.rows.length > 1) {
+        table.deleteRow(1);
+    }
+    generateTable(data, page);
+}
+
+function nextPage() {
+    if (currentPage < Math.ceil(data.length / itemsPerPage)) {
+        currentPage++;
+        showPage(currentPage);
+        updatePageNumber();
+    }
+}
+
+function prevPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        showPage(currentPage);
+        updatePageNumber();
+    }
+}
+
+function updatePageNumber() {
+    document.getElementById("currentPage").textContent = currentPage;
+}
+
 form.addEventListener("submit", function (event) {
     event.preventDefault();
+    currentPage = 1;
     const userID = document.getElementById("userID").value;
     const id = document.getElementById("id").value;
     const title = document.getElementById("title").value;
@@ -14,7 +45,6 @@ form.addEventListener("submit", function (event) {
 });
 
 function addTableRow(userID, id, title, completed) {
-
     const newRow = table.insertRow();
     const cellUserID = newRow.insertCell(0);
     const cellID = newRow.insertCell(1);
@@ -22,7 +52,7 @@ function addTableRow(userID, id, title, completed) {
     const cellCompleted = newRow.insertCell(3);
     const cellAction = newRow.insertCell(4);
 
-    const idExists = Array.from(table.rows).some(row => row.cells[1].innerHTML ===id);
+    const idExists = Array.from(table.rows).some(row => row.cells[1].innerHTML === id);
 
     if (idExists) {
         alert("the same ID already exists!");
@@ -53,23 +83,27 @@ function addTableRow(userID, id, title, completed) {
     cellAction.appendChild(editButton);
 }
 
+function generateTable(data, page) {
+    const startIndex = (page - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    for (let i = startIndex; i < endIndex && i < data.length; i++) {
+        addTableRow(data[i].userId, data[i].id, data[i].title, data[i].completed);
+    }
+}
 
 fetch('https://jsonplaceholder.typicode.com/todos/')
     .then(response => {
         if (response.ok) {
-            return response.json()
+            return response.json();
         }
         throw new Error('something went wrong');
     })
-    .then(data => generateTable(data))
+    .then(dataResponse => {
+        data = dataResponse; // Set the data in the global scope
+        generateTable(data, currentPage);
+        showPage(currentPage);
+        updatePageNumber();
+    })
     .catch(myError);
 
-function myError(error) {
-    console.log(error);
-}
-function generateTable(data) {
-    for (let i = 0; i < data.length; i++) {
-        console.log(data[i].userId, data[i].id, data[i].title, data[i].completed);
-        addTableRow(data[i].userId, data[i].id, data[i].title, data[i].completed);
-    }
-}
--- practise.js
+++ practise.js
@@ -1,4 +1,9 @@
 const a={b:1}
  a['b']=2
  console.log(a)
-//  
+ 
+// changing arrays //
+const bikes= ["Ducati","Benelli","Respol","Bmw"];
+bikes[0]="harley";
+bikes[1]="ducati"
+console.log(bikes)
