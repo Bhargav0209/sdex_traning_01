16e9c272b0137e0cfc16acaf534920b0e0a9a8eb
Bhargav <bhargav@axivise.in>
2023-11-07 16:24:19 +0530

    new code for pagination,delete & put

--- data.js
+++ data.js
@@ -3,28 +3,14 @@ const table = document.getElementById("dataTable");
 
 let currentPage = 1;
 const itemsPerPage = 10;
+let data = []; // Initialize an empty array to store data
 
 function showPage(page) {
+    // Clear all rows except the header row
     while (table.rows.length > 1) {
         table.deleteRow(1);
     }
     generateTable(data, page);
-}
-
-function nextPage() {
-    if (currentPage < Math.ceil(data.length / itemsPerPage)) {
-        currentPage++;
-        showPage(currentPage);
-        updatePageNumber();
-    }
-}
-
-function prevPage() {
-    if (currentPage > 1) {
-        currentPage--;
-        showPage(currentPage);
-        updatePageNumber();
-    }
 }
 
 function updatePageNumber() {
@@ -52,24 +38,29 @@ function addTableRow(userID, id, title, completed) {
     const cellCompleted = newRow.insertCell(3);
     const cellAction = newRow.insertCell(4);
 
-    const idExists = Array.from(table.rows).some(row => row.cells[1].innerHTML === id);
-
-    if (idExists) {
-        alert("the same ID already exists!");
-        return;
-    }
-
     cellUserID.innerHTML = userID;
     cellID.innerHTML = id;
     cellTitle.innerHTML = title;
-    cellCompleted.innerHTML = completed ? '<i class="fa-solid fa-check" style="color: #35b137,font-size: 30px"></i>' : '<i class="fa-solid fa-xmark" style="color: #bf582b;"></i>';
+    cellCompleted.innerHTML = completed
+        ? '<i class="fa-solid fa-check" style="color: #35b137,font-size: 30px"></i>'
+        : '<i class="fa-solid fa-xmark" style="color: #bf582b;"></i>';
 
     const deleteButton = document.createElement("button1");
     deleteButton.innerHTML = 'Delete <i class="fa-solid fa-trash-can" style="color: white;"></i>';
     deleteButton.addEventListener("click", function () {
-        table.deleteRow(newRow.rowIndex);
+        const idToDelete = newRow.cells[1].innerHTML;
+        fetch(`https://mock-api-template-rh6s.onrender.com/users/${idToDelete}`, {
+            method: 'DELETE',
+        })
+            .then(response => {
+                if (response.ok) {
+                    table.deleteRow(newRow.rowIndex);
+                    showPage(currentPage);
+                } else {
+                    // Handle error
+                }
+            });
     });
-
     cellAction.appendChild(deleteButton);
 
     const editButton = document.createElement("button2");
@@ -78,9 +69,48 @@ function addTableRow(userID, id, title, completed) {
         document.getElementById("userID").value = newRow.cells[0].innerHTML;
         document.getElementById("id").value = newRow.cells[1].innerHTML;
         document.getElementById("title").value = newRow.cells[2].innerHTML;
-    });
 
+        form.removeEventListener("submit", handleEditSubmit);
+        form.addEventListener("submit", handleEditSubmit);
+    });
     cellAction.appendChild(editButton);
+
+    function handleEditSubmit(event) {
+        event.preventDefault();
+
+        const editedUserID = document.getElementById("userID").value;
+        const editedID = document.getElementById("id").value;
+        const editedTitle = document.getElementById("title").value;
+        const editedCompleted = document.getElementById("completed").checked;
+
+        fetch(`https://mock-api-template-rh6s.onrender.com/users/${editedID}`, {
+            method: 'PUT', // or 'PATCH' depending on your API
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                userid: editedUserID,
+                id: editedID,
+                title: editedTitle,
+                completed: editedCompleted,
+            }),
+        })
+            .then(response => {
+                if (response.ok) {
+                    newRow.cells[0].innerHTML = editedUserID;
+                    newRow.cells[1].innerHTML = editedID;
+                    newRow.cells[2].innerHTML = editedTitle;
+                    newRow.cells[3].innerHTML = editedCompleted
+                        ? '<i class="fa-solid fa-check" style="color: #35b137,font-size: 30px"></i>'
+                        : '<i class="fa-solid fa-xmark" style="color: #bf582b;"></i>';
+                    showPage(currentPage); // Update the page after editing
+                } else {
+                    // Handle error
+                }
+            });
+
+        form.removeEventListener("submit", handleEditSubmit);
+    }
 }
 
 function generateTable(data, page) {
@@ -88,11 +118,11 @@ function generateTable(data, page) {
     const endIndex = startIndex + itemsPerPage;
 
     for (let i = startIndex; i < endIndex && i < data.length; i++) {
-        addTableRow(data[i].userId, data[i].id, data[i].title, data[i].completed);
+        addTableRow(data[i].userid, data[i].id, data[i].title, data[i].completed);
     }
 }
 
-fetch('https://jsonplaceholder.typicode.com/todos/')
+fetch('https://mock-api-template-rh6s.onrender.com/users')
     .then(response => {
         if (response.ok) {
             return response.json();
@@ -106,4 +136,3 @@ fetch('https://jsonplaceholder.typicode.com/todos/')
         updatePageNumber();
     })
     .catch(myError);
-
